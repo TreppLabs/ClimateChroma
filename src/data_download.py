e9ab717e60a35654ebe4da97f474660f88b1b7d4
Jim Ruppert <ruppert@tetrahedra.com>
2025-01-21 11:58:31 -0800

    Remove .DS_Store and add to .gitignore

---
+++ src/data_download.py
@@ -0,0 +1,36 @@
+
+import os
+import requests
+
+def test_download():
+    return "The data_download module is working correctly!"
+
+import requests
+import os
+
+def hello_noaa():
+    # Get the API token from the environment variable
+    api_token = os.getenv("NOAA_API_TOKEN")
+
+    if not api_token:
+        raise ValueError("NOAA API token not found. Ensure the NOAA_API_TOKEN environment variable is set.")
+    
+    # API endpoint
+    url = "https://www.ncei.noaa.gov/cdo-web/api/v2/datasets"
+    
+    # Headers
+    headers = {
+        "token": api_token
+    }
+    
+    # Make the request
+    response = requests.get(url, headers=headers)
+    
+    # Raise an error if the response status is not 200
+    response.raise_for_status()
+
+    # print(response.json())
+    
+    return response.json()
+    # return "foo"
+
