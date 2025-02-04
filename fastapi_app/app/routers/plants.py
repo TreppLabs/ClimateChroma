265caba61eef35c3d8a228ce85f8f2e206b47e22
Jim Ruppert <ruppert@tetrahedra.com>
2025-02-03 17:46:08 -0800

    got the actual power plant data from db to display

--- fastapi_app/app/routers/plants.py
+++ fastapi_app/app/routers/plants.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-from .. import models, schemas, database
 import logging
+from .. import models, schemas, database
 
 router = APIRouter(
     prefix="/plants",
@@ -16,22 +16,23 @@ def get_db():
     finally:
         db.close()
 
+# NOTE: this function does not get called, but if we delete it we get errors.
+# the functoin get_plants() in ../main.py is called instead.
+# someday figure this out and this up
 @router.get("/", response_model=List[schemas.Plant])
 def get_plants(southWestLat: float, southWestLng: float, northEastLat: float, northEastLng: float, db: Session = Depends(get_db)):
     logging.info(f"Querying plants within bounds: SW({southWestLat}, {southWestLng}), NE({northEastLat}, {northEastLng})")
-    logging.info("Attempting to query the database")
-    logging.info(f"Querying plants within bounds: SW({southWestLat}, {southWestLng}), NE({northEastLat}, {northEastLng})")
     try:
+        plants = [
+            {"plant_name": "Plant C", "latitude": 37.7649, "longitude": -122.4194, "utility_name": "Utility A"},
+            {"plant_name": "Plant D", "latitude": 37.7944, "longitude": -122.2711, "utility_name": "Utility B"}
+        ]
         # plants = db.query(models.Plant).filter(
         #     models.Plant.latitude >= southWestLat,
         #     models.Plant.latitude <= northEastLat,
         #     models.Plant.longitude >= southWestLng,
         #     models.Plant.longitude <= northEastLng
         # ).all()
-        plants = [
-            {"plant_name": "Plant A", "latitude": 37.7749, "longitude": -122.4194, "utility_name": "Utility A"},
-            {"plant_name": "Plant B", "latitude": 37.8044, "longitude": -122.2711, "utility_name": "Utility B"}
-        ]
         if not plants:
             logging.warning("No plants found within the specified bounds.")
             raise HTTPException(status_code=404, detail="No plants found")
